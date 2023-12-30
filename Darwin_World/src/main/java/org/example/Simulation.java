7c67fb729c2374fc1aa51b23fa9e4db0ba3dedf9
Lukasz <lukasz.9@o2.pl>
2023-12-30 16:21:28 +0100

    Changed simulation structure

--- Darwin_World/src/main/java/org/example/Simulation.java
+++ Darwin_World/src/main/java/org/example/Simulation.java
@@ -3,33 +3,20 @@ package org.example;
 import org.example.model.*;
 
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Simulation{
-    private final WorldMap map;
-    private final List<Animal> animalsList = new ArrayList<>();
-    private final int animalEnergy;
-    private final int reproductionEnergy;
-    private final int grassEnergy;
-    private final int readyEnergy;
-    private final int grassNum;
-
+public class Simulation extends AbstractSimulation {
 
     public Simulation(int animalNumbers,int genNumbers, WorldMap map, int animalEnergy, int readyEnergy,
                       int reproductionEnergy, int grassEnergy, int grassNum) {
-        this.map = map;
-        this.reproductionEnergy = reproductionEnergy;
-        this.animalEnergy = animalEnergy;
-        this.grassEnergy = grassEnergy;
-        this.readyEnergy = readyEnergy;
-        this.grassNum = grassNum;
+        super(map, animalEnergy, reproductionEnergy, grassEnergy, readyEnergy, grassNum);
         for(int i=0; i<animalNumbers; ++i){
             Animal animal = new Animal(genNumbers, i, animalEnergy);
             animalsList.add(animal);
             this.map.place(animal);
         }
+        System.out.println("A " +animalsList);
         GrassPositionGenerator grassPositionGenerator = new GrassPositionGenerator(grassNum, map.getCurrentBounds());
         for (Vector2d position : grassPositionGenerator) {
             Grass grass = new Grass(position);
@@ -38,13 +25,12 @@ public class Simulation{
     }
 
     public void run(){
+        DayCycleSimulation dayCycleSimulation = new DayCycleSimulation(animalsList, map, animalEnergy, reproductionEnergy, grassEnergy, readyEnergy, grassNum);
         for(int i=0; i<5; ++i){
-            for(Animal animal : animalsList){
-                System.out.println("Animal"+animal.getID());
-                map.move(animal);
-            }
+            dayCycleSimulation.move();
         }
     }
+
     public List<Animal> getAnimalsList() {
         return Collections.unmodifiableList(animalsList);
     }
