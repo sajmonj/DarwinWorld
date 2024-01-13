5c213e0c14eec98712bce8cc85ceac5b1643435a
Lukasz <lukasz.9@o2.pl>
2024-01-13 21:58:48 +0100

    IO Configuration finished

--- Darwin_World/src/main/java/org/example/visualization/ConfigurationPresenter.java
+++ Darwin_World/src/main/java/org/example/visualization/ConfigurationPresenter.java
@@ -2,7 +2,11 @@ package org.example.visualization;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
-import org.example.data.Configuration;
+import org.example.data.SimulationConfiguration;
+import org.example.model.ConsoleMapDisplay;
+import org.example.model.RectangularMap;
+import org.example.model.WorldMap;
+import org.example.simulation.Simulation;
 
 public class ConfigurationPresenter {
 
@@ -25,15 +29,61 @@ public class ConfigurationPresenter {
     @FXML
     private TextField grassEnergy;
 
-    @FXML
-    private Configuration configuration;
+    private SimulationConfiguration configuration = null;
 
     @FXML
     public void onSimulationStartClicked(){
-        this.configuration = setConfiguration();
+        onSimulationSaveClicked();
+        System.out.println(configuration);
+        WorldMap map = new RectangularMap(configuration.getMapWidth(),configuration.getMapHeight(),1);
+        ConsoleMapDisplay consoleMapDisplay = new ConsoleMapDisplay();
+        map.registerObserver(consoleMapDisplay);
+        Simulation simulation = new Simulation(configuration.getAnimalsNumber(), configuration.getGenNumbers(), map,
+                configuration.getAnimalEnergy(), configuration.getReadyEnergy(), configuration.getReproductionEnergy(),
+                configuration.getGrassEnergy(), configuration.getGrassNum());
+        simulation.run();
     }
-    private Configuration setConfiguration(){
-        return new Configuration(mapHeight,mapWidth,animalsNumber,genNumbers,animalEnergy,
-                readyEnergy,reproductionEnergy, grassNum,grassEnergy);
+    public void onSimulationSaveClicked(){
+        creatConfiguration();
+        setConfiguration();
+        configuration.save();
+    }
+    public void onSimulationLoadClicked(){
+        creatConfiguration();
+        configuration.load();
+        updateConfiguration();
+    }
+
+    private void creatConfiguration() {
+        if(configuration == null){
+            configuration = new SimulationConfiguration();
+        }
+    }
+
+
+    private void setConfiguration(){
+        int height = Integer.parseInt(mapHeight.getText());
+        int width = Integer.parseInt(mapWidth.getText());
+        int animals = Integer.parseInt(animalsNumber.getText());
+        int generations = Integer.parseInt(genNumbers.getText());
+        int energy = Integer.parseInt(animalEnergy.getText());
+        int readyEnergyValue = Integer.parseInt(readyEnergy.getText());
+        int reproductionEnergyValue = Integer.parseInt(reproductionEnergy.getText());
+        int grassNumber = Integer.parseInt(grassNum.getText());
+        int grassEnergyValue = Integer.parseInt(grassEnergy.getText());
+
+        configuration.update(height, width, animals, generations, energy, readyEnergyValue,
+                reproductionEnergyValue, grassNumber, grassEnergyValue);
+    }
+    private void updateConfiguration(){
+        mapHeight.setText(String.valueOf(configuration.getMapHeight()));
+        mapWidth.setText(String.valueOf(configuration.getMapWidth()));
+        animalsNumber.setText(String.valueOf(configuration.getAnimalsNumber()));
+        genNumbers.setText(String.valueOf(configuration.getGenNumbers()));
+        animalEnergy.setText(String.valueOf(configuration.getAnimalEnergy()));
+        readyEnergy.setText(String.valueOf(configuration.getReadyEnergy()));
+        reproductionEnergy.setText(String.valueOf(configuration.getReproductionEnergy()));
+        grassNum.setText(String.valueOf(configuration.getGrassNum()));
+        grassEnergy.setText(String.valueOf(configuration.getGrassEnergy()));
     }
 }
