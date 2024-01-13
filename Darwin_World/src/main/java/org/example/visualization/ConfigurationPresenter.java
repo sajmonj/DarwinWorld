95c44c8598adede503be6ffc385c3ed5c738cd5c
unknown <lukasz.9@o2.pl>
2024-01-13 18:55:46 +0100

    Add IO Configuration

--- Darwin_World/src/main/java/org/example/visualization/ConfigurationPresenter.java
+++ Darwin_World/src/main/java/org/example/visualization/ConfigurationPresenter.java
@@ -1,4 +1,39 @@
 package org.example.visualization;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import org.example.data.Configuration;
+
 public class ConfigurationPresenter {
+
+    @FXML
+    private TextField mapHeight;
+    @FXML
+    private TextField mapWidth;
+    @FXML
+    private TextField animalsNumber;
+    @FXML
+    private TextField genNumbers;
+    @FXML
+    private TextField animalEnergy;
+    @FXML
+    private TextField readyEnergy;
+    @FXML
+    private TextField reproductionEnergy;
+    @FXML
+    private TextField grassNum;
+    @FXML
+    private TextField grassEnergy;
+
+    @FXML
+    private Configuration configuration;
+
+    @FXML
+    public void onSimulationStartClicked(){
+        this.configuration = setConfiguration();
+    }
+    private Configuration setConfiguration(){
+        return new Configuration(mapHeight,mapWidth,animalsNumber,genNumbers,animalEnergy,
+                readyEnergy,reproductionEnergy, grassNum,grassEnergy);
+    }
 }
